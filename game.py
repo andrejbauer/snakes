bd3997ee9466507ab433662ca0d53011e396fdcd
Blaz Blazic <Bblazic4@gmail.com>
2014-11-28 18:16:34 +0100

    # spremenil sem barvo kace in jo dodal med ostale dve

--- game.py
+++ game.py
@@ -12,12 +12,13 @@ import random
 
 from bolivianAnaconda import BolivianAnaconda
 from congoWaterCobra import CongoWaterCobra
-
+from egyptianCobra import EgyptianCobra
 # Nato v spisek SNAKES dodate razred, ki predstavlja vaso kaco
 
 SNAKES = [
     BolivianAnaconda,
     CongoWaterCobra,
+    BolivianAnaconda
 ]
 
 # Od tu naprej se ni treba nicesar dotikati
