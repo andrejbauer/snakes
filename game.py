c0cfc830c2be261e10de77a1ca6ddac2d8a66f1d
Anja Petković <anja.petkovic@gmail.com>
2014-11-28 18:14:05 +0100

    dodala svojo kaco tudi v igrico

--- game.py
+++ game.py
@@ -12,12 +12,14 @@ import random
 
 from bolivianAnaconda import BolivianAnaconda
 from congoWaterCobra import CongoWaterCobra
+from diamondPython import DiamondPython
 
 # Nato v spisek SNAKES dodate razred, ki predstavlja vaso kaco
 
 SNAKES = [
     BolivianAnaconda,
     CongoWaterCobra,
+    DiamondPython,
 ]
 
 # Od tu naprej se ni treba nicesar dotikati
