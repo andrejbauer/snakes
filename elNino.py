60823ad4257d458a7f6efe8ead54138c68f533bb
Nino Basic <nino.basic@gmail.com>
2014-11-28 19:08:19 +0100

    Dodal inteligenco

--- elNino.py
+++ elNino.py
@@ -33,14 +33,66 @@ class ElNino(Snake):
         # Choose closest item
         dist = float('inf')
         goal = None
+
+        if len(self.field.mice) == 0: return
+        
         for food in self.field.mice.keys():
             if self.d(food, self.coords[0]) < dist:
                 dist = self.d(food, self.coords[0])
                 goal = food
 
-        print(self.field.width, self.field.hight)
-        if random.randint(0,10) < 5:
-            if random.randint(0,1) == 1:
-                self.turn_left()
-            else:
-                self.turn_right()
+        w, h = self.field.width, self.field.height
+        grid = [[True for i in range(h)] for j in range(w)]
+        for kaca in self.field.snakes:
+            for u, v in kaca.coords:
+                grid[u][v] = False
+        for u, v in self.coords:
+                grid[u][v] = False
+        x, y = self.coords[0]
+        grid[x][y] = True
+        # print(grid)
+        graph = {(i, j):[] for i in range(1, w) for j in range(1, h)
+                 if grid[i][j] == True}
+
+        for g in graph:
+            for (du, dv) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                sos = (g[0] + du, g[1] + dv)
+                if sos in graph:
+                    graph[g].append(sos)
+
+        # print(graph)
+
+        def vec_plus(a, b):
+            return (a[0] + b[0], a[1] + b[1])
+
+        ne = self.shortest_path(graph, self.coords[0], goal)
+
+        lx, ly = -self.dy, self.dx
+
+        if ne == vec_plus(self.coords[0], (self.dx, self.dy)):
+            pass # do nothing
+        elif ne == vec_plus(self.coords[0], (lx, ly)):
+            self.turn_left()
+        else:
+            self.turn_right()
+        
+    
+    def shortest_path(self, gr, beg, end):
+        # print(gr)
+        # raise KeyError
+        seen = {beg}
+        queue = [beg]
+        parent = {beg: None}
+        while queue:
+            u = queue.pop(0)
+            # print(gr[u])
+            for v in gr[u]:
+                if v in seen: continue
+                parent[v] = u
+                seen.add(v)
+                queue.append(v)
+
+        path = [end]
+        while path[-1] != beg:
+            path.append(parent[path[-1]])
+        return list(reversed(path))[1]
